ca80e183b51bea3243afeea9c3cdb43d6d19c8d7
Oleksiy Liubchenko <alexeylubchenko75@gmail.com>
2023-03-31 21:30:37 +0300

    Add empty views

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+def index(request, pk):
+    pass
+
+
+class GoalListView(generic.ListView):
+    pass
+
+
+class GoalCreateView(generic.CreateView):
+    pass
+
+
+class GoalDetailView(generic.DetailView):
+    pass
+
+
+class GoalStageCreateView(generic.CreateView):
+    pass
+
+
+class HabitListView(generic.ListView):
+    pass
+
+
+class HabitCreateView(generic.CreateView):
+    pass
+
+
+class HabitDetailView(generic.DetailView):
+    pass
+
+
+class NoteCreateView(generic.CreateView):
+    pass
+
+
+class CommentaryCreateView(generic.CreateView):
+    pass
