659be075a32465e18aef3e097f0fcc30cc6fcf9d
Oleksiy Liubchenko <alexeylubchenko75@gmail.com>
2023-03-31 18:05:59 +0300

    Add models

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class Goal(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="goals")
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    deadline = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class GoalStage(models.Model):
+    goal = models.ForeignKey(Goal, on_delete=models.CASCADE, related_name='stages')
+    stage_name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    deadline = models.DateTimeField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Habit(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='habits')
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    month_goal = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Note(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes')
+    text = models.TextField()
+    goals = models.ForeignKey(Goal, on_delete=models.CASCADE, related_name='notes')
+    habits = models.ForeignKey(Habit, on_delete=models.CASCADE, related_name='notes')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Commentary(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    goals = models.ManyToManyField(Goal, related_name='commentaries')
+    habits = models.ManyToManyField(Habit, related_name='commentaries')
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
