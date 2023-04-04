4b3ba849aeca24fd0d2638142f7d5d6b1e93173c
Oleksiy Liubchenko <alexeylubchenko75@gmail.com>
2023-04-04 13:58:39 +0300

    Add superuser test_user

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from tracker.models import Habit, Goal
 
+# SuperUser login - test_user
+# SuperUser pass - 12345678
+
 
 admin.site.register(Habit)
 admin.site.register(Goal)
