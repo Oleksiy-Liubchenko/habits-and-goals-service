60aba502698b24ac6a33f5b893cab2fa7635b22d
Oleksiy Liubchenko <alexeylubchenko75@gmail.com>
2023-04-05 12:05:49 +0300

    Update detail and stage views for goal

--- tracker/views.py
+++ tracker/views.py
@@ -1,10 +1,10 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views import generic
 from django.forms import widgets
 
-from tracker.forms import GoalCreationForm
+from tracker.forms import GoalCreationForm, GoalCreationStageForm
 from tracker.models import (
     Goal,
     GoalStage,
@@ -39,9 +39,24 @@ class GoalDetailView(generic.DetailView):
     model = Goal
     template_name = "goal/goal_detail.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['goal_stages'] = self.object.stages.all()
+        return context
+
 
 class GoalStageCreateView(generic.CreateView):
-    pass
+    model = GoalStage
+    form_class = GoalCreationStageForm
+    #success_url = reverse_lazy("tracker:goal-detail")
+    template_name = "goal/goalstage_form.html"
+
+    def form_valid(self, form):
+        form.instance.goal_id = self.kwargs["goal_id"]
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse("tracker:goal-detail", kwargs={"pk": self.kwargs["goal_id"]})
 
 
 class HabitListView(generic.ListView):
