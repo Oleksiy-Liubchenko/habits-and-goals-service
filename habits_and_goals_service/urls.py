f6db486c2e287d24467c6f48ff7091185c9d9a02
Oleksiy Liubchenko <alexeylubchenko75@gmail.com>
2023-04-12 18:08:09 +0300

    Migrate (add Meta ordering goalstage) & hide urls login/logout

--- habits_and_goals_service/urls.py
+++ habits_and_goals_service/urls.py
@@ -19,9 +19,9 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
     path('', include("tracker.urls")),
-    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path("accounts/", include("django.contrib.auth.urls"))
+    # path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    # path('logout/', LogoutView.as_view(), name='logout'),
 ]
 
