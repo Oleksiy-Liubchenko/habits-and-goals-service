b2404c9ac67e9a3081a7bf61845927918c654a84
Oleksiy Liubchenko <alexeylubchenko75@gmail.com>
2023-04-15 14:45:50 +0300

    Add test_forms.py

---
+++ tests/test_forms.py
@@ -0,0 +1,111 @@
+from django.test import TestCase
+from django import forms
+from django.utils import timezone
+import datetime
+from tracker.forms import (
+    GoalNameSearchForm,
+    GoalCreationForm,
+    GoalCreationStageForm,
+    GoalCommentaryForm,
+    HabitNameSearchForm,
+    HabitCreationForm,
+    HabitCommentaryForm,
+    HabitDayCompletionForm
+)
+
+
+class FormsTest(TestCase):
+    def test_goal_search_by_name_form_widget(self):
+        form = GoalNameSearchForm()
+
+        self.assertIsInstance(
+            form.fields["name"].widget, forms.TextInput
+        )
+
+    def test_goal_creation_form_is_valid(self):
+        deadline = timezone.make_aware(
+            datetime.datetime(2023, 3, 4)
+        )
+        data = {
+            "name": "Test_name",
+            "description": "Test_description",
+            "deadline": deadline
+        }
+
+        form = GoalCreationForm(data=data)
+
+        self.assertTrue(form.is_valid())
+
+    def test_goal_creation_form_without_deadline(self):
+
+        data = {
+            "name": "Test_name",
+            "description": "Test_description",
+            "deadline": ""
+        }
+
+        form = GoalCreationForm(data=data)
+
+        self.assertFalse(form.is_valid())
+
+    def test_goal_stage_creation_form_is_valid(self):
+        deadline = timezone.make_aware(
+            datetime.datetime(2023, 3, 4)
+        )
+        data = {
+            "stage_name": "Test_name",
+            "description": "Test_description",
+            "deadline": deadline
+        }
+
+        form = GoalCreationStageForm(data=data)
+        print(form.errors)
+        self.assertTrue(form.is_valid())
+
+    def test_goal_commentary_form_is_valid(self):
+        data = {"text": "Test commentary"}
+        form = GoalCommentaryForm(data=data)
+        self.assertTrue(form.is_valid())
+
+    def test_goal_commentary_form_blank_text(self):
+        data = {"text": ""}
+        form = GoalCommentaryForm(data=data)
+        self.assertFalse(form.is_valid())
+
+    def test_habit_search_by_name_form_widget(self):
+        form = HabitNameSearchForm()
+
+        self.assertIsInstance(
+            form.fields["name"].widget, forms.TextInput
+        )
+
+    def test_habit_creation_form_is_valid(self):
+        data = {
+            "name": "Test_name",
+            "description": "Test_description",
+            "month_goal": "Test_month_goal"
+        }
+
+        form = HabitCreationForm(data=data)
+
+        self.assertTrue(form.is_valid())
+
+    def test_habit_commentary_form_is_valid(self):
+        data = {"text": "Test commentary"}
+        form = HabitCommentaryForm(data=data)
+        self.assertTrue(form.is_valid())
+
+    def test_habit_commentary_form_blank_text(self):
+        data = {"text": ""}
+        form = HabitCommentaryForm(data=data)
+        self.assertFalse(form.is_valid())
+
+    def test_habit_day_completion_form_is_valid(self):
+        data = {"status": "completed"}
+        form = HabitDayCompletionForm(data=data)
+        self.assertTrue(form.is_valid())
+
+    def test_habit_day_completion_form_blank_status(self):
+        data = {"status": ""}
+        form = HabitDayCompletionForm(data=data)
+        self.assertFalse(form.is_valid())
